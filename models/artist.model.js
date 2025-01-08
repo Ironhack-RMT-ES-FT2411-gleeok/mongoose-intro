0e50082911e75526e96a27b279a3a08e36818d52
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2025-01-08 13:51:34 +0100

    done

---
+++ models/artist.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+// aqui creamos el schema
+const artistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    // unique: true
+  },
+  awardsWon: {
+    type: Number,
+    min: 0
+  },
+  isTouring: Boolean,
+  genre: {
+    type: [String],
+    enum: ["rock", "alternative", "jazz", "folk", "pop", "grunge"]
+  }
+})
+
+// aqui creamos el modelo
+const Artist = mongoose.model("Artist", artistSchema)
+//                                |              |
+//        nombre interno del modelo SINGULAR     | 
+//                                            el esquema
+
+module.exports = Artist
